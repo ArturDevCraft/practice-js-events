f060e43bac69ea2e6f701916312554d87f60548c
Artur <artur.czerwiak@gmail.com>
2025-10-06 15:11:52 +0200

    Task 04 completed

---
+++ 04/app.js
@@ -0,0 +1,29 @@
+const divs = document.querySelectorAll('div');
+const body = document.querySelector('body');
+
+body.addEventListener('click', bodyClickHandler);
+
+divs.forEach((e) => {
+	e.addEventListener('click', clickHandler);
+});
+
+function clickHandler(e) {
+	const time = e.currentTarget.dataset.time;
+
+	setTimeout(
+		(e) => {
+			e.classList.add('clicked');
+			console.log(e);
+		},
+		time,
+		e.currentTarget
+	);
+}
+
+function bodyClickHandler(e) {
+	if (e.target === e.currentTarget) {
+		divs.forEach((e) => {
+			e.classList.remove('clicked');
+		});
+	}
+}
